c5e719a3b3b0dd01ec56f6ae63de7e00d499d4ed
bobbyjonesPro <serranod7@students.rowan.edu>
2017-11-01 20:53:34 -0400

    Update KnightsTour.java

--- KnightsTour.java
+++ KnightsTour.java
@@ -1,4 +1,3 @@
-
 public class KnightsTour {
 	
 	public static Integer[][] startKnightsTour(int size, int currX, int currY){
@@ -10,14 +9,26 @@ public class KnightsTour {
 		while(result == false && attempt < 8){
 			result = knightsTour(map,currX,currY,2,attempt++);
 		}
+		//if (result == false){
+		//	return null;
+		//}
 		return map;
 			
 	}
 
 	private static boolean knightsTour(Integer[][] map, int currX, int currY, int turn, int attempt){
+		int x = 0;
+		int y = 0;
 		if(attempt == 1){
 			try{
-				map[currX-2][currY+1] = turn;
+				if (map[currX-2][currY+1] == null){
+					map[currX-2][currY+1] = turn;
+					x = currX-2;
+					y = currY+1;
+				}
+				else{
+					return false;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -25,7 +36,15 @@ public class KnightsTour {
 		}
 		else if(attempt == 2){
 			try{
-				map[currX-2][currY-1] = turn;
+				if (map[currX-2][currY-1] == null){
+					map[currX-2][currY-1] = turn;
+					x = currX-2;
+					y = currY-1;
+					System.out.println(turn);
+				}
+				else{
+					return false;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -33,7 +52,15 @@ public class KnightsTour {
 		}
 		else if(attempt == 3){
 			try{
-				map[currX+2][currY+1] = turn;
+				if (map[currX+2][currY+1] == null){
+					map[currX+2][currY+1] = turn;
+					x = currX+2;
+					y = currY+1;
+					System.out.println(turn + " " + attempt);
+				}
+				else{
+					return false;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -41,7 +68,15 @@ public class KnightsTour {
 		}
 		else if(attempt == 4){
 			try{
-				map[currX+2][currY-1] = turn;
+				if (map[currX+2][currY-1] == null){
+					map[currX+2][currY-1] = turn;
+					x = currX+2;
+					y = currY-1;
+					
+				}
+				else{
+					return false;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -49,7 +84,14 @@ public class KnightsTour {
 		}
 		else if(attempt == 5){
 			try{
-				map[currX+1][currY-2] = turn;
+				if(map[currX+1][currY-2] == null){
+					map[currX+1][currY-2] = turn;
+					x = currX+1;
+					y = currY-2;
+				}
+				else{
+					return false;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -57,7 +99,11 @@ public class KnightsTour {
 		}
 		else if(attempt == 6){
 			try{
-				map[currX-1][currY-2] = turn;
+				if(map[currX-1][currY-2] == null){
+					map[currX-1][currY-2] = turn;
+					x = currX-1;
+					y = currY-2;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -65,7 +111,14 @@ public class KnightsTour {
 		}
 		else if(attempt == 7){
 			try{
-				map[currX+1][currY+2] = turn;
+				if(map[currX+1][currY+2] == null){
+					map[currX+1][currY+2] = turn;
+					x = currX+1;
+					y = currY+2;
+				}
+				else{
+					return false;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -73,7 +126,14 @@ public class KnightsTour {
 		}
 		else if(attempt == 8){
 			try{
-				map[currX-1][currY+2] = turn;
+				if(map[currX-1][currY+2] == null){
+					map[currX-1][currY+2] = turn;
+					x = currX-1;
+					y = currY+2;
+				}
+				else{
+					return false;
+				}
 			}
 			catch(Exception e){
 				return false;
@@ -84,11 +144,26 @@ public class KnightsTour {
 			return true;
 		}
 		else{
-			boolean result = knightsTour(map,currX,currY,turn++,1);
+			boolean result = knightsTour(map,x,y,turn+1,1);
 			int tries = 1;
 			
 			while(result == false && tries < 8){
-				result = knightsTour(map,currX,currY,2,tries++);
+				result = knightsTour(map,x,y,turn+1,tries++);
+				
+				if(tries == 8){
+					System.out.println(turn + "\t" + tries);
+					
+					for(int i = 0; i < 5; i++) {
+						map[x][y] = null;
+						for(int j = 0; j < 5; j++) {
+							System.out.print(map[j][i] + "\t");
+						}
+						System.out.println(" \n\n");
+					}
+				}
+			}
+			if (result == false && tries == 8){
+				//map[x][y] = null;
 			}
 			return false;
 		}
